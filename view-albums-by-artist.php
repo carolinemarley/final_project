365925c847609aa28b15ff8617b92502bd1a7941
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-04 13:40:39 -0600

    Update view-albums-by-artist.php

--- view-albums-by-artist.php
+++ view-albums-by-artist.php
@@ -15,8 +15,8 @@
 while ($album = $albums->fetch_assoc()) {
 ?>
   <tr>
-    <td> <?php echo $album['album_id']?></td>
     <td> <?php echo $album['album_title']?></td>
+    <td> <?php echo $album['albums_on_hand']?></td>
     <td> <?php echo $album['price']?></td>
     <td> <?php echo $album['release_year']?></td>
     <td> <?php echo $album['genre_name']?></td>
