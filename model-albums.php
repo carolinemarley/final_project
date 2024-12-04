ff58ec3576777dac0a18911a8f2060f2a71a36f3
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-04 11:06:44 -0600

    Update model-albums.php

--- model-albums.php
+++ model-albums.php
@@ -2,7 +2,7 @@
 function selectAlbums() {
   try {
     $conn = get_db_connection();
-    $stmt = $conn->prepare("SELECT album_id, album_name, albums_on_hand, price, release_year FROM `album`");
+    $stmt = $conn->prepare("SELECT album_id, album_title, albums_on_hand, price, release_year FROM `album`");
     $stmt->execute(); 
     $result = $stmt->get_result();
     $conn->close(); 
