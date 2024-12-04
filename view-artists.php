4843c28cef0c1e2fdeaa9ea18179864326945941
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-04 10:12:56 -0600

    Update view-artists.php

--- view-artists.php
+++ view-artists.php
@@ -1,15 +1,16 @@
 <h1>Artists</h1>
-<div class="table-responsive">
-  <table class="table">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Stage Name</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
+<div class="container">
+  <div class="table-responsive">
+    <table class="table table-striped table-bordered table-hover">
+      <thead class="thead-dark">
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Stage Name</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
 <?php
 while ($artist = $artists->fetch_assoc()) {
   ?>
