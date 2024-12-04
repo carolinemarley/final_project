2222f0e5d619199993f13cdbfd5df72bed0cc9b5
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-04 11:43:29 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,8 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-<style>
-    body {
-    background-image: url('C:/Users/Caroline Marley/Desktop/final_project_image.jpg');
-        background-size: cover;
-        background-position: center center; 
-        background-attachment: fixed; 
-    }
-</style>
     <h1>Final Project</h1>
+<img src = "C:\Users\Caroline Marley\Desktop\final_project_image.jpg">
 <?php
 include "view-footer.php";
 ?>
