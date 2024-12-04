50f67dacb575cbd99eb471d4744189cfb8ec6823
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-04 12:13:38 -0600

    Update view-artists-with-albums.php

--- view-artists-with-albums.php
+++ view-artists-with-albums.php
@@ -5,7 +5,7 @@ while ($artist = $artists->fetch_assoc()) {
   ?>
     <div class="card">
     <div class="card-body">
-      <h5 class="card-title"><?php echo $artist['artist_name'];?></h5>
+      <h5 class="card-title"><?php echo $artist['genre_name'];?></h5>
       <p class="card-text">
       <ul class="list-group">
   <?php
