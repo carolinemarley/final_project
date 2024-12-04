17dc4d808821cba7aa9f4001f013ff8b50ef64c3
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-04 10:59:53 -0600

    Create view-artists-with-albums.php

---
+++ view-artists-with-albums.php
@@ -0,0 +1,27 @@
+<h1>Artists with Albums</h1>
+<div class="card-group">
+<?php
+while ($artist = $artists->fetch_assoc()) {
+  ?>
+    <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?php echo $artist['artist_name'];?></h5>
+      <p class="card-text">
+      <ul class="list-group">
+  <?php
+    $books = selectAlbumsByArtist($artist['artist_id']);
+  while ($album = $albums->fetch_assoc()){
+?>
+     <li class="list-group-item"><?php echo $album['album_title']?> - <?php echo $album['albums_on_hand']?> - <?php echo $album['price']?> - <?php echo $album['release_year']?> - <?php echo $album['genre_name']?> </li>
+<?php
+  }
+  ?>
+      </ul>
+      </p>
+      <p class="card-text"><small class="text-body-secondary">Genre: <?php echo $artist['stage_name'];?></small></p>
+    </div>
+    </div>
+  <?php
+}
+?>
+</div>
