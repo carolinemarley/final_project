b9524516a9d5320418b00877befb3ab32ae7f3d2
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-04 12:07:49 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -31,6 +31,9 @@
         <li class="nav-item">
           <a class="nav-link" href="analysis.php">Analysis</a>
          </li>
+        <li class="nav-item">
+          <a class="nav-link" href="location.php">Location</a>
+         </li>
       </ul>
     </div>
   </div>
