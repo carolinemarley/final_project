9cdbfc4f7ffac9d16eb720f699cc9cc10d197b19
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-04 10:33:10 -0600

    Create albums.php

---
+++ albums.php
@@ -0,0 +1,10 @@
+<?php 
+require_once("util-db.php"); 
+require_once("model-albums.php");
+
+$pageTitle = "Albums";
+include "view-header.php";
+$albums = selectAlbums(); 
+include "view-albums.php";
+include "view-footer.php";
+?>
