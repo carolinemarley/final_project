4d1c327efe8081d1f1446f2f78f15db7daad707d
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-04 11:44:51 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
     <h1>Final Project</h1>
-<img src = "C:\Users\Caroline Marley\Desktop\final_project_image.jpg">
+<img src = "images/final_project_image.jpg">
 <?php
 include "view-footer.php";
 ?>
