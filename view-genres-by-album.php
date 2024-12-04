cdb836f77d266b19f5b71bb85cbef1d6e5a728bc
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-04 11:05:25 -0600

    Create view-genres-by-album.php

---
+++ view-genres-by-album.php
@@ -0,0 +1,31 @@
+<h1>Genres by Album</h1>
+<div class = "table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Albums on Hand</th>
+        <th>Price</th>
+        <th>Release Year</th>
+        <th>Genre Name </th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+while ($genre = $genres->fetch_assoc()) {
+?>
+  <tr>
+    <td> <?php echo $genre['album_id']?></td>
+    <td> <?php echo $genre['album_title']?></td>
+    <td> <?php echo $genre['albums_on_hand']?></td>
+    <td> <?php echo $genre['price']?></td>
+    <td> <?php echo $genre['release_year']?></td>
+    <td> <?php echo $genre['genre_name']?></td>
+  </tr>
+  <?php
+}
+?>
+    </tbody>
+  </table>
+</div>
