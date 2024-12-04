8e87cb1ce127ee0a7a4617ad971c59390798e6ec
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-04 11:48:00 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,7 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
     <h1>Final Project</h1>
-<img src="https://thevinylfactory.com/wp-content/uploads/2016/03/Phonica_cover.jpg" alt="Phonica Records">
-
+<img src="https://capitolfile.com/get/files/image/galleries/record-shop-mick-haupt-unsplash.jpg" alt="Record Shop Image">
 <?php
 include "view-footer.php";
 ?>
