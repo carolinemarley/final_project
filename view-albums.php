1a09a200c1495bd43fa2fa044de940316c4de104
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-11 14:21:15 -0600

    Update view-albums.php

--- view-albums.php
+++ view-albums.php
@@ -1,5 +1,13 @@
 <body style = "background-color: #f8c9d4;">
-<h1>Albums</h1>
+<div class = "row">
+  <div class = "col">
+    <h1>Albums</h1>
+  </div>
+  <div class = "col-auto">
+  <?php
+include "view-albumss-newform.php";
+?>
+  </div>
   <div class = "container">
 <div class = "table-responsive">
   <table class="table table-striped table-bordered table-hover">
@@ -24,8 +32,25 @@ while ($album = $albums->fetch_assoc()) {
    <td> <?php echo $album['price']?></td>
    <td> <?php echo $album['release_year']?></td>
     <td>
+      <?php
+  include "view-albums-editform.php";
+      ?>
+ </td>
+    <td>
+    <form method = "post" action = "">
+      <input type = "hidden" name = "aid" value = "<?php echo $album['album_id']?>">
+      <input type = "hidden" name = "actionType" value = "Delete">
+      <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+          <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+        </svg>
+      </button>
+    </form>
+    </td>
+    <td>
           <form method = "post" action = "genres-by-album.php">
-            <input type = "hidden" name = "bid" value = "<?php echo $album['genre_id']?>">
+            <input type = "hidden" name = "aid" value = "<?php echo $album['album_id']?>">
       <button type="submit" class="btn btn-primary">Genres</button>
     </form>
     </td>
@@ -36,4 +61,3 @@ while ($album = $albums->fetch_assoc()) {
     </tbody>
   </table>
 </div>
-</body>
