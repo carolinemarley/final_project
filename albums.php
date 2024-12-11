51ff706ca6537f3d78a8f08942e194e106ed6b3b
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-11 14:05:47 -0600

    Update albums.php

--- albums.php
+++ albums.php
@@ -4,6 +4,33 @@ require_once("model-albums.php");
 
 $pageTitle = "Albums";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']) {
+    case "Add":
+      if (insertAlbum($_POST['aTitle'], $_POST['aOnHand'], $_POST['aPrice'], $_POST['aYear'])) {
+          echo '<div class="alert alert-success" role="alert">Album Added!</div>';
+      } else {
+           echo '<div class="alert alert-danger" role="alert">ERROR!</div>';
+      }
+      break;
+        case "Edit":
+      if (updateAlbum($_POST['aTitle'], $_POST['aOnHand'], $_POST['aPrice'], $_POST['aYear'], $_POST['aid'])) {
+          echo '<div class="alert alert-success" role="alert">Album Edited!</div>';
+      } else {
+           echo '<div class="alert alert-danger" role="alert">ERROR!</div>';
+      }
+      break;
+         case "Delete":
+      if (deleteAlbum($_POST['aid'])) {
+          echo '<div class="alert alert-success" role="alert">Album deleted!</div>';
+      } else {
+           echo '<div class="alert alert-danger" role="alert">ERROR!</div>';
+      }
+      break;
+    }
+    }
+
 $albums = selectAlbums(); 
 include "view-albums.php";
 include "view-footer.php";
