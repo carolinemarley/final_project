974f70112386c6299e67dff7b1799725a3e332ba
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-12 12:08:21 -0600

    Update model-genres-by-album.php

--- model-genres-by-album.php
+++ model-genres-by-album.php
@@ -2,7 +2,7 @@
 function selectGenresbyAlbum($aid) {
   try {
     $conn = get_db_connection();
-    $stmt = $conn->prepare("SELECT album_id, album_title, genre_name, a.genre_id FROM album a JOIN genre g on a.genre_id = g.genre_id where a.genre_id = ?");
+    $stmt = $conn->prepare("SELECT album_id, album_title, genre_name, a.genre_id FROM album a JOIN genre g on a.genre_id = g.genre_id where a.album = ?");
     $stmt->bind_param("i", $aid);
     $stmt->execute(); 
     $result = $stmt->get_result();
