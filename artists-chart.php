d361878377bd47dbdc97a49108bd33b356ffe7fc
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-11 20:19:11 -0600

    Update artists-chart.php

--- artists-chart.php
+++ artists-chart.php
@@ -1,10 +1,16 @@
 <?php
 require_once("util-db.php");
 require_once("model-artists-chart.php"); 
-
 $pageTitle = "Artists Chart";
 include "view-header.php";
-$artists = selectArtists();
+
+try {
+    $artists = selectArtists();
+} catch (Exception $e) {
+    echo "Error retrieving artists: " . $e->getMessage();
+    exit;
+}
+
 include "js1.php";
 include "view-footer.php";
 ?>
