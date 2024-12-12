94988b278b6e34eff967c88b967df628ea24523b
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-11 19:50:02 -0600

    Update js1.php

--- js1.php
+++ js1.php
@@ -1,7 +1,36 @@
 <h1>Artists Chart</h1>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: data = {
+    datasets: [{
+        data: [
 <?php
 while ($artist = $artists->fetch_assoc()) {
-
+  echo $artist['num_genres'] . ",";
 }
-?>
+?> 
+        ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+<?php
+$artists = selectArtists(); 
+while ($artist = $artists->fetch_assoc()) {
+  echo "'" .$artist['artist_name'] . "',";
+}
+?> 
+    ]
+}, 
+  });
+</script>
   
