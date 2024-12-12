0505b7a10d11062c330c7a8030d44182c6605467
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-11 20:18:42 -0600

    Update model-artists-chart.php

--- model-artists-chart.php
+++ model-artists-chart.php
@@ -2,13 +2,21 @@
 function selectArtists() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT artist_name, count(g.artist_id) as num_genres FROM `artist` a join `genre` g on a.artist_id = g.artist_id group by artist_name");
+        $stmt = $conn->prepare("SELECT artist_name, count(g.artist_id) as num_genres FROM artist a JOIN genre g ON a.artist_id = g.artist_id GROUP BY artist_name");
         $stmt->execute();
         $result = $stmt->get_result();
+        
+        $artists = [];
+        while ($row = $result->fetch_assoc()) {
+            $artists[] = $row;
+        }
+        
         $conn->close();
-        return $result;
+        return $artists;
     } catch (Exception $e) {
-        $conn->close();
+        if ($conn) {
+            $conn->close();
+        }
         throw $e;
     }
 }
