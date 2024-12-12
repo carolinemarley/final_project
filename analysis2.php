536bcb8ea61c7411c4670e3c6b53bcf17ddefc06
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-12 15:14:21 -0600

    Update analysis2.php

--- analysis2.php
+++ analysis2.php
@@ -1,16 +1,16 @@
 <?php
 require_once("util-db.php");
-require_once("model-analysis2.php"); 
+require_once("model-analysis.php"); 
 
-$pageTitle = "Albums Chart";
+$pageTitle = "Artists Chart";
 include "view-header.php";
-$albums = selectAlbums();
+$artists = selectArtists();
 
-if ($albums == null) {
-  echo "Error: No albums data available.";
+if ($artists == null) {
+  echo "Error: No artists data available.";
   exit; 
 }
 
-include "view-analysis2.php";
+include "view-analysis.php";
 include "view-footer.php";
 ?>
