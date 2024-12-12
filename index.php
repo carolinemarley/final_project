7dd2b9d9a1bee03d9cbff46dd124642de5b93b92
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-12 12:31:26 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,10 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
     <h1 style = "color: lightgray;">Final Project</h1>
+
+<p style="color: white; font-size: 18px; text-align: center;">
+    Welcome to the Homepage for our record store. We're glad you are visitng our page and thank you for you interest!
+</p>
 <style>
     body {
         background-image: url('https://capitolfile.com/get/files/image/galleries/record-shop-mick-haupt-unsplash.jpg');
