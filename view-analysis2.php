41a7292b96949af93a27409d1c6bed460eaf8b1b
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-12 15:23:09 -0600

    Update view-analysis2.php

--- view-analysis2.php
+++ view-analysis2.php
@@ -1,51 +1,35 @@
-<body style="background-color: #f8c9d4;">
-  <h1>Bar Analysis</h1>
-  <div>
-    <canvas id="myChart"></canvas>
-  </div>
-  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<body style = "background-color: #f8c9d4;">
+<h1>Doughnut Analysis</h1>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-  <script>
-    const ctx = document.getElementById('myChart');
 
-    new Chart(ctx, {
-      type: 'bar',  
-      data: {
-        datasets: [{
-          label: 'Number of Albums',  
-          data: [
-            <?php
-            while ($artist = $artists->fetch_assoc()) {
-              echo $artist['num_albums'] . ",";
-            }
-            ?> 
-          ],
-          backgroundColor: '#FF5733',  
-          borderColor: '#C70039',      
-          borderWidth: 1
-        }],
-        labels: [
-          <?php
-          $artists = selectArtists(); 
-          while ($artist = $artists->fetch_assoc()) {
-            echo "'" . $artist['album_title'] . "',";  
-          }
-          ?> 
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: data = {
+    datasets: [{
+        data: [
+<?php
+while ($artist = $artists->fetch_assoc()) {
+  echo $artist['num_albums'] . ",";
+}
+?> 
         ]
-      },
-      options: {
-        responsive: true,
-        scales: {
-          y: {
-            beginAtZero: true  
-          }
-        },
-        plugins: {
-          legend: {
-            display: false  
-          }
-        }
-      }
-    });
-  </script>
+    }],
 
+    labels: [
+<?php
+$artists = selectArtists(); 
+while ($artist = $artists->fetch_assoc()) {
+  echo "'" .$artist['stage_name'] . "',";
+}
+?> 
+    ]
+}, 
+  });
+</script>
