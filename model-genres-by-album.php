afdd1f9b43a9a973737168743ed1a651c651ae89
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-12 12:16:20 -0600

    Update model-genres-by-album.php

--- model-genres-by-album.php
+++ model-genres-by-album.php
@@ -1,5 +1,5 @@
 <?php
-function selectGenresbyAlbum($aid) {
+function selectGenresbyAlbum($genre_id) {
   try {
     $conn = get_db_connection();
     $stmt = $conn->prepare("select album_id, album_title, genre_name, a.genre_id from album a join genre g on a.genre_id = g.genre_id where a.genre_id = ?");
