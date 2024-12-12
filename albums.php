09ec407e2942bbc25e5d81e8f7b4c81e876664b7
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-12 11:44:50 -0600

    Update albums.php

--- albums.php
+++ albums.php
@@ -8,7 +8,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
     switch ($_POST['actionType']) {
     case "Add":
-      if (insertAlbum($_POST['aTitle'], $_POST['aOnHand'], $_POST['aPrice'], $_POST['aYear'])) {
+      if (insertAlbum($_POST['aTitle'], $_POST['aOnHand'], $_POST['aPrice'], $_POST['aYear'], $_POST['aGenre'])) {
           echo '<div class="alert alert-success" role="alert">Album Added!</div>';
       } else {
            echo '<div class="alert alert-danger" role="alert">ERROR!</div>';
