bfbf7503c0b3f660621fca4609a01d5c4e2450c4
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-12 12:16:41 -0600

    Update genres-by-album.php

--- genres-by-album.php
+++ genres-by-album.php
@@ -4,7 +4,7 @@ require_once("model-genres-by-album.php");
 
 $pageTitle = "Genres by Album";
 include "view-header.php";
-$genres = selectGenresByAlbum($_POST['aid']); 
+$genres = selectGenresByAlbum($_POST['genre_id']); 
 include "view-genres-by-album.php";
 include "view-footer.php";
 ?>
