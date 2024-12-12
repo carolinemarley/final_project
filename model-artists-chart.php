34fcc93d70913d54a8b5ba9a00ccbfb24da90a3e
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-11 20:26:28 -0600

    Update model-artists-chart.php

--- model-artists-chart.php
+++ model-artists-chart.php
@@ -2,7 +2,7 @@
 function selectArtists() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT artist_name, count(g.artist_id) as num_genres FROM `artist` a join `genre` g on a.artist_id = g.artist_id group by artist_name");
+        $stmt = $conn->prepare("SELECT stage_name, count(al.artist_id) as num_albums FROM `artist` a join `album` al on a.artist_id = al.artist_id group by stage_name");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
