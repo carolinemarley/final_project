93e24f812b9ed6575ac8a95f0d0bfd94b695ba05
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-12 14:45:31 -0600

    Update model-analysis2.php

--- model-analysis2.php
+++ model-analysis2.php
@@ -6,10 +6,7 @@ function selectArtists() {
             throw new Exception('Database connection failed.');
         }
 
-        $stmt = $conn->prepare("SELECT stage_name, count(al.artist_id) as num_albums 
-                                FROM `artist` a 
-                                JOIN `album` al ON a.artist_id = al.artist_id 
-                                GROUP BY stage_name");
+        $stmt = $conn->prepare("select stage_name, COUNT(albums_on_hand) as num_albums from artist a join album al on a.artist_id = al.artist_id group by stage_name");
 
         // Execute the query and check if it's successful
         if (!$stmt->execute()) {
