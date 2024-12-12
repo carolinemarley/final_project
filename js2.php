59ff39a10ed749e0a4e16218fb4c4ca16325a01d
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-11 18:29:32 -0600

    Create js2.php

---
+++ js2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alert</title>
+</head>
+<body>
+    <button onclick="showAlert()">Click This Button</button>
+    <script>
+        function showAlert() {
+            alert("This is my alert using JavaScript!");
+        }
+    </script>
+</body>
+</html>
