10860d645d83cc902a3e4e664a03e6cc8fe9170d
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-12 11:17:46 -0600

    Create view-analysis.php

---
+++ view-analysis.php
@@ -0,0 +1,34 @@
+<h1>Artists Chart</h1>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: data = {
+    datasets: [{
+        data: [
+<?php
+while ($artist = $artists->fetch_assoc()) {
+  echo $artist['num_albums'] . ",";
+}
+?> 
+        ]
+    }],
+
+    labels: [
+<?php
+$artists = selectArtists(); 
+while ($artist = $artists->fetch_assoc()) {
+  echo "'" .$artist['stage_name'] . "',";
+}
+?> 
+    ]
+}, 
+  });
+</script>
