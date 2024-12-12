554c874445015db19858ed0252d49ba80cd738b9
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-11 21:09:26 -0600

    Update model-artists.php

--- model-artists.php
+++ model-artists.php
@@ -12,4 +12,48 @@ function selectArtists() {
         throw $e;
     }
 }
+
+function insertArtist($aName, $aSName) {
+  try {
+    $conn = get_db_connection();
+    $stmt = $conn->prepare("INSERT INTO `artist` (`artist_name`, `stage_Name`) VALUES (?, ?)");
+    $stmt->bind_param("ss", $aName, $aSName);
+    $success = $stmt->execute(); 
+    $result = $stmt->get_result(); 
+    $conn->close(); 
+    return $success; 
+  } catch (Exception $e){
+      $conn->close();
+    throw $e;
+  }
+}
+
+function updateArtist($aName, $aSName, $aid) {
+  try {
+    $conn = get_db_connection();
+    $stmt = $conn->prepare("UPDATE `artist` set `artist_name` = ?, `stage_name` =?  WHERE artist_id = ?");
+    $stmt->bind_param("ssi", $aName, $aSName, $aid);
+    $success = $stmt->execute(); 
+    $conn->close(); 
+    return $success; 
+  } catch (Exception $e){
+      $conn->close();
+    throw $e;
+  }
+}
+
+function deleteArtist($aid) {
+  try {
+    $conn = get_db_connection();
+    $stmt = $conn->prepare("delete from artist where artist_id =?");
+    $stmt->bind_param("i", $aid);
+    $success = $stmt->execute(); 
+    $conn->close(); 
+    return $success; 
+  } catch (Exception $e){
+      $conn->close();
+    throw $e;
+  }
+}
+?>
 ?>
