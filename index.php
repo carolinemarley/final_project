54398f5faba7c0a7f0669f0190c242a3b596dba3
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-12 14:12:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,12 @@ include "view-header.php";
 ?>
     <h1 style = "color: white;">Final Project</h1>
 
-<p style="color: lightblue; font-size: 18px; text-align: center;">
+<p style="color: red; font-size: 18px; text-align: center;">
     Welcome to the Homepage for our record store. We're glad you are visitng our page and thank you for you interest!
 </p>
 
- <h2 style = "color: lightblue;">About the Store</h2>
-    <p style="color: lightblue; font-size: 18px; text-align: center;">
+ <h2 style = "color: red;">About the Store</h2>
+    <p style="color: red; font-size: 18px; text-align: center;">
        We are located in Norman, Oklahoma at 125 E Main St. Norman, OK 73069. You can see the exact location on the location page. 
         We were founded in 2020 with the hopes of selling records to music lovers all over. We just want to bring out passion for music to those around us. 
         We are dedicated to serving our customers right and the community of Norman. 
