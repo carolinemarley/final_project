ea6e46b1c158011a6c498d5fcd2fab5653591ae4
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-11 18:20:49 -0600

    Create js.php

---
+++ js.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Animation Example</title>
+</head>
+<body>
+    <div id="box" style="width:100px;height:100px;background-color:red;"></div>
+    <button onclick="moveBox()">Move</button>
+    <script>
+        function moveBox() {
+            var box = document.getElementById("box");
+            box.style.position = "relative";
+            box.style.left = "100px";
+        }
+    </script>
+</body>
+</html>
