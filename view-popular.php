a22474dfe7011a6df5a69d31fda7f336f261135b
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-12 19:57:36 -0600

    Update view-popular.php

--- view-popular.php
+++ view-popular.php
@@ -1,1 +1,13 @@
 <body style = "background-color: #f8c9d4;">
+<h1> Most Popular Albums for Sale</h1>
+
+<div class="card" style="width: 18rem;">
+  <img src="https://th.bing.com/th/id/OIF.CPoRTMy0MbtlG04rhDAIeQ?rs=1&pid=ImgDetMain" class="card-img-top" >
+  <div class="card-body">
+    <p class="card-text">
+      The Secret of Us <br><br>
+      Gracie Abrams <br><br>
+      2024
+    </p>
+  </div>
+</div>
