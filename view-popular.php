5c0ab8f35f929265fb9261696228d2ac50542b28
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-12 19:59:04 -0600

    Update view-popular.php

--- view-popular.php
+++ view-popular.php
@@ -3,7 +3,7 @@
 
 <div class="card" style="width: 18rem;">
   <img src="https://th.bing.com/th/id/OIF.CPoRTMy0MbtlG04rhDAIeQ?rs=1&pid=ImgDetMain" class="card-img-top" >
-  <div class="card-body">
+  <div class="card-body text-center>
     <p class="card-text">
       The Secret of Us <br><br>
       Gracie Abrams <br><br>
